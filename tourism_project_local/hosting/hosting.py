27a486e5b3f9d40f70ec69f087401fa10ed243e7
Chandrashekhara Kundadka <chandraBE@gmail.com>
2026-07-07 15:29:22 +0000

    Uploading commit

--- tourism_project_local/hosting/hosting.py
+++ tourism_project_local/hosting/hosting.py
@@ -2,10 +2,13 @@ from huggingface_hub import HfApi
 import os
 repo_id = "Chandrashekhara/tourism-project"
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+token = os.getenv("HF_TOURISM")
+api = HfApi(token=token)
+
 api.upload_folder(
     folder_path="tourism_project_local/deployment",     # the local folder containing your files
     repo_id=repo_id,                              # the target repo
     repo_type="space",                            # dataset, model, or space
-    path_in_repo="",                              # optional: subfolder path inside the repo
+    path_in_repo="",                               # optional: subfolder path inside the repo
+    token=token
 )
